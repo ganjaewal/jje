714c00b56a456b3fd9c3cfb92676918996764c51
ganjaewal <jdm050610@gmail.com>
2024-10-15 23:58:18 +0900

    버튼삭제

--- src/chapter16/calculator.java
+++ src/chapter16/calculator.java
@@ -6,9 +6,6 @@ public class calculator extends JFrame {
     calculator() {
         setTitle("계산기");
 
-        JButton b = new JButton("버튼");
-        add(b);
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(250, 300);
         setVisible(true);
